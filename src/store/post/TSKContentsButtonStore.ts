fc6bdd10156aefd3e36f397a9fa830f8b06b35e4
thovy <kingvely151113@gmail.com>
2023-11-09 13:51:30 +0900

    feat: 공고 등록 페이지에서 사용되는 job 선택 후 관련 tsk 선택 버튼에 사용되는 store
    - zustand 를 사용해 store를 만들었습니다.
    - job 선택 후 task, skill, knowledge 의 선택에 따라 다른 item을 보여줄 수 있도록.

---
+++ src/store/post/TSKContentsButtonStore.ts
@@ -0,0 +1,16 @@
+import { create } from 'zustand';
+import { JobContents, TaskContents, KnowledgeContents, SkillContents } from '../../components/post/posting/dummyJob';
+
+type TSKContentsButtonStore = {
+    selectedJobContents: JobContents | null;
+    selectedItemType: 'tasks' | 'skills' | 'knowledges' ;
+    selectJobContents: (jobContents: JobContents) => void;
+    selectItemType: (itemType: 'tasks' | 'skills' | 'knowledges') => void;
+}
+
+export const useTSKContentsButtonStore = create<TSKContentsButtonStore>((set) => ({
+    selectedJobContents: null,
+    selectedItemType: "tasks",
+    selectJobContents: (jobContents) => set(() => ({ selectedJobContents: jobContents })),
+    selectItemType: (itemType) => set(() => ({ selectedItemType: itemType })),
+}));
