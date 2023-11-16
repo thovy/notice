3859613133d623bb16aed7434fd9d0a84e07197c
thovy <kingvely151113@gmail.com>
2023-11-16 14:22:31 +0900

    feat: header를 만들었습니다.
    - header를 만들었습니다.
    - header로 인해 윗부분이 가려지기 때문에 본문의 padding을 조금 더 줬습니다.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <div className="header-container">
+        <div className="header-wrapper">
+          <div className="header-logo">
+            <p>Logo</p>
+          </div>
+          <div className="header-menu-wrapper">
+            <div className="header-menu">
+              <a href="/job">채용공고</a>
+            </div>
+            <div className="header-menu">
+              직무분석
+            </div>
+          </div>
+          <div className="header-user">
+            <div className="header-user">
+              <a href="/login">로그인</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
