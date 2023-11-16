0a2c26a0d60c848eb37fbba460eb3458b0e3ca52
thovy <kingvely151113@gmail.com>
2023-11-16 09:50:52 +0900

    style: jobpostlist 페이지의 스타일 구현
    - jobpostlist 의 스타일을 만들고 스타일 구현을 위한 구성 수정

--- src/pages/post/JobPostList.tsx
+++ src/pages/post/JobPostList.tsx
@@ -1,6 +1,8 @@
 import React, { useMemo, useState } from 'react'
 import JobPostThumb from '../../components/post/list/JobPostThumb'
 import { Post, dummyPost, JobContents, dummyJob } from '../../components/post/dummyJob'
+import './JobPostList.css'
+import { Link } from 'react-router-dom'
 
 const JobPostList = () => {
 
@@ -32,50 +34,65 @@ const JobPostList = () => {
 
   return (
     <>
-        <div className="post-list-title-container">
-            <div className="title-wrapper">
-                <p>채용공고 리스트</p>
+        <div className="post-list-container">
+            <div className="post-list-title-container">
+                <div className="title-wrapper">
+                    <p>채용공고 리스트</p>
+                </div>
+                <div className="regist-post-button-wrapper">
+                    <Link to={'/job/posting'}>
+                    <button
+                        className="regist-post"
+                    >
+                    채용공고 등록
+                    </button>
+                    </Link>
+                </div>
             </div>
-            <div className="regist-post-button-wrapper">
-                <button className="regist-post">채용공고 등록</button>
+            <div className="view-button-container">
+                <div className="view-button-wrapper">
+                    <button
+                        className={`view-button ${selectedView === 'total' ? 'active' : ''}`}
+                        onClick={()=>setSelectedView('total')}
+                    >전체</button>
+                    <button
+                        className={`view-button ${selectedView === 'job' ? 'active' : ''}`}
+                        onClick={()=>setSelectedView('job')}
+                    >직무별</button>
+                </div>
             </div>
-        </div>
-        <div className="view-button-container">
-            <div className="view-button-wrapper">
-                <button className="view-button" onClick={()=>setSelectedView('total')} >전체</button>
-                <button className="view-button" onClick={()=>setSelectedView('job')}>직무별</button>
-            </div>
-        </div>
 
-        {/* 직무별일때 직무 선택 드롭다운메뉴 */}
-        {selectedView === 'job' &&
-        <div className="job-drop-down-wrapper">
-            <select 
-                className='job-drop-down'
-                onChange={(e) => setSelectedJob(e.target.value)}
-            >
-                <option value={"-1"} >직무를 선택해주세요.</option>
-                {jobData.map((jobContent:any) => (
-                    <option key={jobContent.id} value={jobContent.id}>
-                        {jobContent.title}
-                    </option>
+            {/* 직무별일때 직무 선택 드롭다운메뉴 */}
+            {selectedView === 'job' &&
+            <div className="job-drop-down-wrapper">
+                <select 
+                    className='job-drop-down'
+                    onChange={(e) => setSelectedJob(e.target.value)}
+                >
+                    <option value={"-1"} >직무를 선택해주세요.</option>
+                    {jobData.map((jobContent:any) => (
+                        <option key={jobContent.id} value={jobContent.id}>
+                            {jobContent.title}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            }
+
+            {/* 직무별일때 선택된 직무에 대한 설명 */}
+
+            <div className="post-list-contents-container">
+                {/* 스크롤을 내리면 더 많은 로딩을 하는 무한스크롤 적용해보기 */}
+                { !filteredPostList ?
+                <div className="nullPostList">
+                    <p>채용공고가 없습니다.</p>
+                </div>
+                :filteredPostList.map((postData)=> (
+                    // 게시글 미리보기 카드
+                    // ispublic 이 true 인 postData만 보여주기
+                    postData.isPublic && <JobPostThumb postData={postData}/>
                 ))}
-            </select>
-        </div>
-        }
-
-        {/* 직무별일때 선택된 직무에 대한 설명 */}
-
-        <div className="post-list-contents-container">
-            {/* 스크롤을 내리면 더 많은 로딩을 하는 무한스크롤 적용해보기 */}
-            { !filteredPostList ?
-            <div className="nullPostList">
-                <p>채용공고가 없습니다.</p>
             </div>
-            :filteredPostList.map((postData)=> (
-                // 게시글 미리보기 카드
-                <JobPostThumb postData={postData} />
-            ))}
         </div>
     </>
   )
