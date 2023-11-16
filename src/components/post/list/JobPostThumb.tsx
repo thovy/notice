71fef8f233e515d0f65475fa4e5eea77c96ca3f4
thovy <kingvely151113@gmail.com>
2023-11-16 09:41:01 +0900

    feat: JobPostList에 나타날 thumbnail 카드들 수정
    - css 조금 수정
    - 날짜 표시 형태 수정

--- src/components/post/list/JobPostThumb.tsx
+++ src/components/post/list/JobPostThumb.tsx
@@ -1,44 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Post } from '../dummyJob'
+import { Link } from 'react-router-dom';
+import JobPostListModal from './JobPostListModal';
+import './JobPostThumb.css'
 
 interface ChildCompoenentProps {
   postData: Post;
 }
 
 const JobPostThumb:React.FC<ChildCompoenentProps> = ({postData}) => {
-    if (!postData) return null;
+    // 모달 상태 관리
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
+    
     const careerString = ['경력 무관', '신입', '경력'];
     const eduString = ['학력 무관', '고졸 이상', '대졸 이상', '석사 이상', '박사 이상'];
 
+    const formatDate = (date: Date) => {
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+
+        return `${year}년 ${month}월 ${day}일`;
+    }
+    
+    const handleModalOpen = () => {
+        setIsModalOpen(!isModalOpen);
+    }
+    
+    if (!postData) return null;
+
   return (
     <>
-        <div className="author-title-container">
-            <div className="author-wrapper">
-                <p>{postData.author}</p>
+        <div className="card-container">
+            <div className="author-title-container">
+                <div className="title-detail-wrapper">
+                    <div className="author-wrapper">
+                        <p>{postData.author}</p>
+                    </div>
+                    <div className="go-detail">
+                        {/* 전체보기라는 글자를 누르면 /job/post/:id 로 이동 */}
+                        <Link to={`/job/post/${postData.id}`}>전체보기 👉</Link>
+                    </div>
+                </div>
+                <div className="title-wrapper">
+                    <p>{postData.title}</p>
+                </div>
             </div>
-            <div className="title-wrapper">
-                <p>{postData.title}</p>
-            </div>
-            <div className="go-detail">
-                <p>전체보기</p>
-            </div>
-        </div>
-        <div className="post-basic-wrapper">
-            <div className="location">
-                <p>위치</p>
-            </div>
-            <div className="newbie">
-                <p>{careerString[postData.career]}</p>
-            </div>
-            <div className="school">
-                <p>{eduString[postData.edu]}</p>
-            </div>
+            <div className="post-basic-wrapper">
+                <div className="location">
+                    <p>근무지 : </p>
+                </div>
+                <div className="newbie">
+                    <p>{careerString[postData.career]}</p>
+                </div>
+                <div className="school">
+                    <p>{eduString[postData.edu]}</p>
+                </div>
                 {postData.startDate && postData.endDate ?
                 <div className="date">
-                    <p>{postData.startDate?.toString()}</p>
+                    <p>{formatDate(postData.startDate)}</p>
                     <p>~</p>
-                    <p>{postData.endDate?.toString()}</p>
+                    <p>{formatDate(postData.endDate)}</p>
                 </div>
                 :
                 <div className="date">
@@ -46,19 +69,23 @@ const JobPostThumb:React.FC<ChildCompoenentProps> = ({postData}) => {
                 </div>
                 }
             </div>
-            <div className="post-main-container">
-                <div className="main-wrapper">
+            <div className="post-analyze-container">
+                <div className="analyze-wrapper">
                     <div className="analyze-result">
                         {postData.isJob == 'job'
                         ? <p>직무 100%</p>
                         : <p>{postData.analyzeResult}</p>
                         }
                     </div>
-                    <div className="open-job-detail">
-                        <button>상세보기</button>
-                    </div>
+                </div>
+            </div>
+            <div className="open-job-detail">
+                <button onClick={()=>handleModalOpen()}>직무 상세보기</button>
             </div>
         </div>
+        {isModalOpen &&
+            <JobPostListModal handleModal={handleModalOpen} postData={postData} />
+        }
     </>
   )
 }
