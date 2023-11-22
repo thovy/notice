f59bca8ae13bbf2518ee7be30fe5d2206690fe5b
thovy <kingvely151113@gmail.com>
2023-11-22 11:16:39 +0900

    feat: login 시 localstorage에 있는 데이터를 사용하도록 구현

--- src/pages/user/Login.tsx
+++ src/pages/user/Login.tsx
@@ -1,8 +1,29 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './Login.css'
+import { useUserStore } from '../../store/user/UserDataStore';
 
 const Login = () => {
+
+    const navigate = useNavigate();
+
+
+    // const {
+    //     setAccount,
+    //     setUsername,
+    //     setIsEnt,
+    //     setLocation,
+    //     setCareer,
+    //     setEdu,
+    //     setCompanyDescription,
+    //     setApplyList,
+    //     setBookmarkList,
+    //     setSkills,
+    //     setKnowledges,
+    // } = useUserStore();
+
+
+    const [userType, setUserType] = useState("일반회원")
 
     const [loginFormData, setLoginFormData] = useState({
         account: '',
@@ -27,25 +48,62 @@ const Login = () => {
         const checkResult:boolean = await checkForm();
 
         if (checkResult){
-            console.log(loginFormData);
-        
-        // const response = await fetch('http://localhost:8000/api/login/', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(loginFormData),
-        // }).then((res) => res.json())
-        // .then((data) => {
-        //     console.log(data);
-        // })
-        // .catch((err) => {
-        //     console.error(err);
-        // })
+
+            var reqUrl = "";
+            if (userType === "일반회원"){
+                console.log("====user login");
+                reqUrl = "/api/login/";
+                
+            } else {
+                console.log("====ent login");
+                reqUrl = "/api/ent/login/";
+            }
+
+            // server api 가 완성되면 fetch api로 바꿔야합니다.
+            // const response = await fetch(`${process.env.REACT_APP_BASE_URL}`+reqUrl, {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify(loginFormData),
+            // }).then((res) => res.json())
+            // .then((data) => {
+            //     console.log(data);
+            // })
+            // .catch((err) => {
+            //     console.error(err);
+            // })
+
+            const userListData = JSON.parse(localStorage.getItem('userListData') || '[]');
+            
+            if (loginFormData.account === "test" && loginFormData.password === "test" && userType === "일반회원"){
+                // localstorage에 있는 userListData에서 account가 test인 user를 찾아서 가져옵니다.
+                const normalUser = userListData.find((user:any) => user.account === "test");
+                console.log(userListData);
+                
+                console.log(normalUser);
+                
+                sessionStorage.setItem("userData", JSON.stringify(normalUser));
+                alert("일반 회원 로그인 성공");
+                navigate('/');
+                // 새로고침 안 하니까 헤더가 안 바뀝니다.
+                window.location.reload();
+            }
+            else if (loginFormData.account === "enttest" && loginFormData.password === "test" && userType === "기업회원"){
+                const entUser = userListData.find((user:any) => user.account === "enttest");
+                sessionStorage.setItem("userData", JSON.stringify(entUser));
+                alert("기업 회원 로그인 성공")
+                navigate('/');
+                // 새로고침 안 하니까 헤더가 안 바뀝니다.
+                window.location.reload();
+            }
+            else{
+                alert("아이디 혹은 비밀번호가 틀렸습니다.")
+            }
+
 
         }
     }
-
 
   return (
     <>
@@ -57,6 +115,16 @@ const Login = () => {
                         <p>로그인</p>
                     </div>
                     <div className="login-input-wrapper">
+                        <div className="typeSelect-wrapper">
+                            <select
+                                value={userType}
+                                onChange={(e) => setUserType(e.target.value)}
+                            >
+                                <option value="일반회원" >일반회원</option>
+                                <option value="기업회원" >기업회원</option>
+                            </select>
+                        </div>
+
                         <div className="login-id-wrapper">
                             <label htmlFor="account">아이디</label>
                             <input
@@ -94,6 +162,13 @@ const Login = () => {
                     >회원가입
                     </button>
                 </Link>
+                <Link to={'/ent/signup'}>
+                    <button
+                        className="regist-button ent"
+                    >기업용 회원가입
+                    </button>
+                </Link>
+                
             </div>
         </div>
     </>
