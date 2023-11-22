f156b74c73aeb31652a7d21099b6781d002be57f
thovy <kingvely151113@gmail.com>
2023-11-22 13:33:50 +0900

    refactor: footer import 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
